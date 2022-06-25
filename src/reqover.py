78f76c74b8e0742ee41dff4b5b6ae84b3ef79656
s.pirohov <sergey.pirogov@gmail.com>
2022-06-25 15:22:31 +0300

    add reqover results uploader

--- src/reqover.py
+++ src/reqover.py
@@ -12,8 +12,46 @@ import sys
 import uuid
 from urllib import parse
 
+import requests
 
-def parse_url_args(url):
+
+def wrap(response):
+    req = response.request
+
+    query_parameters = __parse_url_args(req.url)
+    u = parse.urlparse(req.url)
+    result = {
+        "uuid": str(uuid.uuid4()),
+        "path": u.path,
+        "method": req.method,
+        "statusCode": f"{response.status_code}",
+        "parameters": query_parameters,
+        "body": req.body,
+    }
+
+    __save_result(result)
+
+    return response
+
+
+def create_build(data, token):
+    res = requests.post(f"http://localhost:3000/{token}/builds", files={"file": ""}, data=data)
+    return res.json()['resultsPath']
+
+
+def upload_results(results_url, root_folder=os.path.join(sys.path[0], 'reqover-results')):
+    for path in os.listdir(root_folder):
+        if not path.endswith(".json"):
+            continue
+
+        absolute_file_path = os.path.join(root_folder, path)
+        print(path)
+        with open(absolute_file_path, "r") as f:
+            data = json.loads(f.read())
+            requests.post(results_url, json=data)
+
+
+def __parse_url_args(url):
     query = parse.parse_qs(parse.urlparse(url).query)
     results = []
     for k, v in query.items():
@@ -25,26 +63,7 @@ def parse_url_args(url):
     return results
 
 
-def reqover(response):
-    req = response.request
-
-    query_parameters = parse_url_args(req.url)
-    u = parse.urlparse(req.url)
-    result = {
-        "uuid": str(uuid.uuid4()),
-        "path": u.path,
-        "method": req.method,
-        "statusCode": f"{response.status_code}",
-        "parameters": query_parameters,
-        "body": req.body,
-    }
-
-    save_result(result)
-
-    return response
-
-
-def save_result(result, path=None):
+def __save_result(result, path=None):
     root_dir = sys.path[0]
     results_dir = path
     if not results_dir:
--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,29 +1,37 @@
-import json
-import os
-import sys
-
 import pytest
 import requests
-from urllib import parse
 
-import uuid as uuid
-
-from src.reqover import reqover
+from src.reqover import wrap, upload_results, create_build
 
 BASE_URL = 'https://petstore.swagger.io'
+
+
 # BASE_URL = 'http://localhost:3000/zscsguwimxhe/swagger'
+
+@pytest.fixture(scope="session", autouse=True)
+def setup():
+    yield
+    token = "3oyersu9nazj"
+    data = {
+        "name": "PR-1256",
+        "serviceUrl": "https://petstore.swagger.io",
+        "swaggerUrl": "https://petstore.swagger.io/v2/swagger.json",
+        "basePath": "/v2",
+    }
+    results_url = create_build(data, token)
+    upload_results(results_url)
 
 
 @pytest.mark.parametrize("id", [1, 2, 3])
 def test_can_get_pet_by_id(id):
-    print(reqover(requests.get(f"{BASE_URL}/v2/pet/{id}")).json())
+    print(wrap(requests.get(f"{BASE_URL}/v2/pet/{id}")).json())
 
 
 @pytest.mark.parametrize("status", ["available", "sold", "net", "sold, available"])
 def test_can_get_pet_by_status(status):
-    print(reqover(requests.get(f"{BASE_URL}/v2/pet/findByStatus", params={"status": status})).json())
+    print(wrap(requests.get(f"{BASE_URL}/v2/pet/findByStatus", params={"status": status})).json())
 
 
 @pytest.mark.parametrize("id", [10])
 def test_can_delete_pet_by_id(id):
-    reqover(requests.delete(f"{BASE_URL}/v2/pet/{id}"))
+    wrap(requests.delete(f"{BASE_URL}/v2/pet/{id}"))
