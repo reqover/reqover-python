967ea6288f11d045d5e589692b3675c9c6777983
s.pirohov <sergey.pirogov@gmail.com>
2022-07-07 11:27:29 +0300

    wrap -> cover

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -3,7 +3,7 @@ import os
 import pytest
 import requests
 
-from src.reqover import wrap, upload_results, create_build
+from src.reqover import cover, upload_results, create_build
 
 BASE_URL = 'https://petstore.swagger.io'
 
@@ -13,27 +13,27 @@ BASE_URL = 'https://petstore.swagger.io'
 @pytest.fixture(scope="session", autouse=True)
 def setup():
     yield
-    project_token = "tkonot9rxpm8"
+    project_token = "jjpvondvslsb"
     data = {
         "name": os.getenv("BRANCH", "Master"),
         "serviceUrl": "https://petstore.swagger.io",
         "swaggerUrl": "https://petstore.swagger.io/v2/swagger.json",
         "basePath": "/v2",
     }
-    results_url = create_build("https://reqover-io.herokuapp.com", data, project_token)
+    results_url = create_build("http://localhost:3000", data, project_token)
     upload_results(results_url)
 
 
 @pytest.mark.parametrize("id", [1, 2, 3, None])
 def test_can_get_pet_by_id(id):
-    print(wrap(requests.get(f"{BASE_URL}/v2/pet/{id}")).json())
+    print(cover(requests.get(f"{BASE_URL}/v2/pet/{id}")).json())
 
 
 @pytest.mark.parametrize("status", ["available", "sold", "net", "sold, available"])
 def test_can_get_pet_by_status(status):
-    print(wrap(requests.get(f"{BASE_URL}/v2/pet/findByStatus", params={"status": status})).json())
+    print(cover(requests.get(f"{BASE_URL}/v2/pet/findByStatus", params={"status": status})).json())
 
 
 @pytest.mark.parametrize("id", [10])
 def test_can_delete_pet_by_id(id):
-    wrap(requests.delete(f"{BASE_URL}/v2/pet/{id}"))
+    cover(requests.delete(f"{BASE_URL}/v2/pet/{id}"))
